1d59701100e214c628cbd868b8a677f09edb1bdc
ikrzywda <krzywdaigor890@gmail.com>
2023-10-24 22:38:43 +0200

    Initial Commit

---
+++ HackerNewsSwift/HackerNewsSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsSwiftApp.swift
+//  HackerNewsSwift
+//
+//  Created by Igor Krzywda on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
